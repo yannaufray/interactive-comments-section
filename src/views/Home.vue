78e5a58794580bcd49a1ebd92de8060509d77205
yannbrds <yann.aufray@protonmail.com>
2022-06-21 18:50:50 +0200

    HTML basis

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,44 @@
-<template></template>
+<template>
+  <div class="comment">
+    <div class="infos">
+      <img
+        src="../assets/images/avatars/image-amyrobson.png"
+        alt=""
+        class="pic"
+      />
+      <span class="pseudo">amyrobson</span>
+      <span class="date">1 month ago</span>
+    </div>
+    <div class="text">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam dolorum
+      consequatur recusandae earum suscipit similique sed molestias praesentium
+      ipsum ex fugiat voluptas atque eveniet, itaque commodi dicta enim aut.
+      Quaerat!
+    </div>
+    <div class="interact">
+      <div class="likes">
+        <img src="../assets/images/icon-minus.svg" alt="" />
+        <span>12</span>
+        <img src="../assets/images/icon-plus.svg" alt="" />
+      </div>
+      <div class="reply">
+        <img src="../assets/images/icon-reply.svg" alt="" />
+      </div>
+      <span>Reply</span>
+    </div>
+  </div>
+</template>
 
 <script>
 export default {
   name: "Home",
   components: {},
   setup() {
-    fetch("http://localhost:5000/comments").then((res) =>
-      console.log(res.json())
-    );
+    const comments = fetch("http://localhost:5000/comments").then((res) => {
+      if (res.status === 200) {
+        const data = res.json();
+      }
+    });
   },
 };
 </script>

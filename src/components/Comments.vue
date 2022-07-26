acfe4bcb9e0dd97d3fe9c86653fd75d4a6be23c6
yannbrds <yann.aufray@protonmail.com>
2022-07-26 22:16:48 +0200

    + Comments component (complete reorganisation)

---
+++ src/components/Comments.vue
@@ -0,0 +1,188 @@
+<template>
+  <Teleport to="body">
+    <Modal
+      @delete="handleDelete(idToBeDeleted)"
+      @cancel="this.modalVisible = false"
+      v-if="modalVisible"
+    />
+  </Teleport>
+  <div v-for="comment in comments" :key="comment.id">
+    <Comment
+      @reply="handleReply"
+      @delete="displayDeleteModal"
+      :comment="comment"
+      :currentUser="currentUser"
+      :replyingTo="replyingTo"
+    />
+    <Transition name="fade"
+      ><NewComment
+        @send="handleSend"
+        v-show="replying && comment.id === replyingId"
+        :currentUser="currentUser"
+        :pic="pic"
+    /></Transition>
+
+    <div v-if="comment.replies && comment.replies.length" class="replies">
+      <div v-for="comment in comment.replies" :key="comment.id">
+        <Comment
+          @reply="handleReply"
+          @delete="displayDeleteModal"
+          :comment="comment"
+          :currentUser="currentUser"
+          :replyingTo="replyingTo"
+          :replying="replying"
+        />
+        <Transition name="fade">
+          <NewComment
+            @send="handleSend"
+            v-show="replying && comment.id === replyingId"
+            :currentUser="currentUser"
+            :pic="pic"
+          />
+        </Transition>
+      </div>
+    </div>
+  </div>
+  <Transition name="fade">
+    <NewComment v-show="!replying" @send="handleSend" :pic="pic" />
+  </Transition>
+</template>
+
+<script>
+import Comment from "../components/Comment.vue";
+import NewComment from "../components/NewComment.vue";
+import Modal from "../components/Modal.vue";
+
+export default {
+  components: { Comment, NewComment, Modal },
+  props: {
+    currentUser: { type: String, required: true },
+    pic: { type: String, required: true },
+  },
+  data() {
+    return {
+      comments: [],
+      replying: false,
+      replyingId: null,
+      replyingTo: "",
+      idToBeDeleted: null,
+    };
+  },
+  watch: {
+    comment(oldVal, newVal) {
+      if (newVal.length) {
+        this.$emit(loaded);
+      }
+    },
+  },
+  methods: {
+    fetchComments: function () {
+      fetch("http://localhost:5000/comments")
+        .then((res) => res.json())
+        .then((data) => (this.comments = data))
+        .catch((err) => console.log(err.message));
+    },
+    handleReply: function (commentToBeReplied) {
+      // Toggling off the bottom comment input box
+      this.replying = !this.replying;
+      // Setting id to display input box bellow the right comment
+      this.replyingId = commentToBeReplied.id;
+      // Setting replyingTo to get the @username tag
+      this.replyingTo = commentToBeReplied.user.username;
+    },
+
+    handleSend: async function (content) {
+      const reply = {
+        id: Math.floor(Math.random() * 100000),
+        content: content,
+        createdAt: new Date(),
+        score: 1,
+        replyingTo: this.replyingTo,
+        user: {
+          image: {
+            png: `../assets/images/avatars/image-${this.currentUser}.png`,
+            webp: `../assets/images/avatars/image-${this.currentUser}.webp`,
+          },
+          username: this.currentUser,
+        },
+      };
+
+      !this.replying
+        ? this.addNewComment(reply)
+        : this.addReplyToComment(reply);
+    },
+    addNewComment: async function (reply) {
+      // Updating the DOM
+      this.comments.push(reply);
+
+      // Posting the new data
+      await fetch("http://localhost:5000/comments/", {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify(reply),
+      });
+    },
+    addReplyToComment: async function (reply) {
+      // Updating the DOM
+      const comIndex = this.comments.findIndex(
+        (com) => com.id === this.replyingId
+      );
+      this.comments[comIndex].replies.push(reply);
+
+      // Updating the data with one more reply
+      this.patchComment(this.comments[comIndex]);
+
+      // Resets
+      this.replying = false;
+      this.replyingTo = undefined;
+    },
+    displayDeleteModal: function (id) {
+      this.modalVisible = true;
+      this.idToBeDeleted = id;
+    },
+    handleDelete: async function (id) {
+      this.modalVisible = false;
+
+      const isReply = !this.comments.some((el) => el.id === id);
+
+      !isReply ? this.deleteComment(id) : this.deleteReplyToComment(id);
+    },
+    deleteComment: async function (id) {
+      const comment = this.comments.find((com) => com.id === id);
+
+      fetch(`http://localhost:5000/comments/${comment.id}`, {
+        method: "DELETE",
+        headers: {
+          "Content-type": "application/json",
+        },
+      });
+      this.comments = this.comments.filter((el) => el.id !== id);
+    },
+    deleteReplyToComment: async function (id) {
+      const comment = this.comments.find((com) =>
+        com.replies.some((reply) => reply.id === id)
+      );
+      const toDeletedId = comment.replies.filter((el) => el.id === id);
+      comment.replies.splice(comment.replies.indexOf(toDeletedId), 1);
+
+      this.patchComment(comment);
+    },
+    patchComment: function (comment) {
+      fetch(`http://localhost:5000/comments/${comment.id}`, {
+        method: "PATCH",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify(comment),
+      });
+    },
+  },
+  mounted() {
+    this.fetchComments();
+  },
+};
+</script>
+
+<style></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,191 +1,37 @@
 <template>
-  <Teleport to="body">
-    <Modal
-      @delete="handleDelete(idToBeDeleted)"
-      @cancel="this.modalVisible = false"
-      v-if="modalVisible"
-    />
-  </Teleport>
-  <div v-if="!comments.length">
+  <!-- <div v-if="!loaded">
     <TheLoader />
-  </div>
-  <div v-else class="comments">
+  </div> -->
+  <div class="comments">
     <Profiles
       @profile-changed="changeCurrentProfile"
       :currentUser="currentUser"
     />
-    <div v-for="comment in comments" :key="comment.id">
-      <Comment
-        @reply="handleReply"
-        @delete="displayDeleteModal"
-        :comment="comment"
-        :currentUser="currentUser"
-        :replyingTo="replyingTo"
-      />
-      <transition name="fade"
-        ><NewComment
-          @send="handleSend"
-          v-show="replying && comment.id === replyingId"
-          :currentUser="currentUser"
-          :pic="pic"
-      /></transition>
-
-      <div v-if="comment.replies && comment.replies.length" class="replies">
-        <div v-for="comment in comment.replies" :key="comment.id">
-          <Comment
-            @reply="handleReply"
-            @delete="displayDeleteModal"
-            :comment="comment"
-            :currentUser="currentUser"
-            :replyingTo="replyingTo"
-            :replying="replying"
-          />
-          <transition name="fade">
-            <NewComment
-              @send="handleSend"
-              v-show="replying && comment.id === replyingId"
-              :currentUser="currentUser"
-              :pic="pic"
-            />
-          </transition>
-        </div>
-      </div>
-    </div>
-    <transition name="fade">
-      <NewComment v-show="!replying" @send="handleSend" :pic="pic" />
-    </transition>
+    <Comments @loaded="loaded = true" :currentUser="currentUser" :pic="pic" />
   </div>
 </template>
 
 <script>
-import Comment from "../components/Comment.vue";
+import Comments from "../components/Comments.vue";
 import Profiles from "../components/Profiles.vue";
-import NewComment from "../components/NewComment.vue";
-import Modal from "../components/Modal.vue";
 import TheLoader from "../components/TheLoader.vue";
 
 export default {
   name: "Home",
-  components: { Comment, Profiles, NewComment, Modal, TheLoader },
+  components: { Comments, Profiles, TheLoader },
   data() {
     return {
-      comments: [],
+      loaded: false,
       currentUser: "juliusomo",
-      replying: false,
-      replyingId: null,
-      replyingTo: "",
+
       pic: "",
       modalVisible: false,
-      idToBeDeleted: null,
     };
   },
   mounted() {
     this.pic = require(`../assets/images/avatars/image-${this.currentUser}.png`);
-    this.fetchComments();
   },
   methods: {
-    fetchComments: function () {
-      fetch("http://localhost:5000/comments")
-        .then((res) => res.json())
-        .then((data) => (this.comments = data))
-        .catch((err) => console.log(err.message));
-    },
-    handleReply: function (commentToBeReplied) {
-      // Toggling off the bottom comment input box
-      this.replying = !this.replying;
-      // Setting id to display input box bellow the right comment
-      this.replyingId = commentToBeReplied.id;
-      // Setting replyingTo to get the @username tag
-      this.replyingTo = commentToBeReplied.user.username;
-    },
-
-    handleSend: async function (content) {
-      const reply = {
-        id: Math.floor(Math.random() * 100000),
-        content: content,
-        createdAt: new Date(),
-        score: 1,
-        replyingTo: this.replyingTo,
-        user: {
-          image: {
-            png: `../assets/images/avatars/image-${this.currentUser}.png`,
-            webp: `../assets/images/avatars/image-${this.currentUser}.webp`,
-          },
-          username: this.currentUser,
-        },
-      };
-
-      !this.replying
-        ? this.addNewComment(reply)
-        : this.addReplyToComment(reply);
-    },
-    addNewComment: async function (reply) {
-      // Updating the DOM
-      this.comments.push(reply);
-
-      // Posting the new data
-      await fetch("http://localhost:5000/comments/", {
-        method: "POST",
-        headers: {
-          "Content-type": "application/json",
-        },
-        body: JSON.stringify(reply),
-      });
-    },
-    addReplyToComment: async function (reply) {
-      // Updating the DOM
-      const comIndex = this.comments.findIndex(
-        (com) => com.id === this.replyingId
-      );
-      this.comments[comIndex].replies.push(reply);
-
-      // Updating the data with one more reply
-      this.patchComment(this.comments[comIndex]);
-
-      // Resets
-      this.replying = false;
-      this.replyingTo = undefined;
-    },
-    displayDeleteModal: function (id) {
-      this.modalVisible = true;
-      this.idToBeDeleted = id;
-    },
-    handleDelete: async function (id) {
-      this.modalVisible = false;
-
-      const isReply = !this.comments.some((el) => el.id === id);
-
-      !isReply ? this.deleteComment(id) : this.deleteReplyToComment(id);
-    },
-    deleteComment: async function (id) {
-      const comment = this.comments.find((com) => com.id === id);
-
-      fetch(`http://localhost:5000/comments/${comment.id}`, {
-        method: "DELETE",
-        headers: {
-          "Content-type": "application/json",
-        },
-      });
-      this.comments = this.comments.filter((el) => el.id !== id);
-    },
-    deleteReplyToComment: async function (id) {
-      const comment = this.comments.find((com) =>
-        com.replies.some((reply) => reply.id === id)
-      );
-      const toDeletedId = comment.replies.filter((el) => el.id === id);
-      comment.replies.splice(comment.replies.indexOf(toDeletedId), 1);
-
-      this.patchComment(comment);
-    },
-    patchComment: function (comment) {
-      fetch(`http://localhost:5000/comments/${comment.id}`, {
-        method: "PATCH",
-        headers: {
-          "Content-type": "application/json",
-        },
-        body: JSON.stringify(comment),
-      });
-    },
     changeCurrentProfile: function (profile) {
       this.currentUser = profile.name;
       this.pic = require(`../assets/images/avatars/image-${this.currentUser}.png`);
